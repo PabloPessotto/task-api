0bd918af9634270d7400ac7c79307e3b1e01dd66
Pablo Pessotto <pablo@sparkag.com.br>
2024-04-04 15:53:57 -0300

    add column index table tasks

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Models\Tasks;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
 
     public function index()
     {
-        $tasks = Tasks::all();
+        $userId = Auth::guard('api')->user()->getAuthIdentifier();
+        $tasks = Tasks::where("userId", $userId)->orderByRaw('-`index` desc')->orderBy('index', 'ASC')->get()->toArray();
         $message['text'] = 'All taks returned.';
         $message['status'] = 200;
         return response()->json(["content" => $tasks, 'message' => $message]);
@@ -19,11 +21,12 @@ class TaskController extends Controller
 
     public function store(Request $request)
     {
+        $userId = Auth::guard('api')->user()->getAuthIdentifier();
         $tasks = new Tasks();
         $tasks->title = $request->title;
         $tasks->description = $request->description;
         $tasks->status = $request->status;
-        $tasks->userId = $request->userId;
+        $tasks->userId = $userId;
         $tasks->date = $request->date;
 
         $labels = $request->input('label');
@@ -84,6 +87,41 @@ class TaskController extends Controller
             $task->save();
             $taskUpdated = Tasks::find($id);
             $message['text'] = 'Task updated';
+            $message['status'] = 201;
+            return response()->json(['message' => $message, "content" => $taskUpdated], 201);
+        } else {
+            $message['text'] = 'Task not found.';
+            $message['status'] = 404;
+            return response()->json(['message' => $message, 'content' => null], 404);
+        }
+    }
+
+    public function updateIndex(Request $request, $id)
+    {
+        if (Tasks::where('id', $id)->exists()) {
+            $task = Tasks::find($id);
+            $task->index = is_null($request->index) ? $task->index : $request->index;
+            $task->save();
+            $maxIndex = $task->max('index');
+
+            // $userId = Auth::guard('api')->user()->getAuthIdentifier();
+            // $tasks = Tasks::where("userId", $userId)->get();
+
+            // foreach ($tasks as $taskFor) {
+            //     if ($taskFor->id != $id) {
+            //         if ($taskFor->index < $request->index) {
+            //             $taskFor->index = ++$maxIndex;
+            //             $taskFor->save();
+            //         } else {
+            //             $taskFor->index = --$maxIndex;
+            //             $taskFor->save();
+            //         }
+
+            //     }
+            // }
+
+            $taskUpdated = Tasks::find($id);
+            $message['text'] = 'Task index updated';
             $message['status'] = 201;
             return response()->json(['message' => $message, "content" => $taskUpdated], 201);
         } else {
